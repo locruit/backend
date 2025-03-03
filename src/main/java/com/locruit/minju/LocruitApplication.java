5168ae04ba460b2d57e17773dda19fd860b9a47f
MinJu Kim <rorrxr@naver.com>
2025-03-03 21:55:30 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/locruit/minju/LocruitApplication.java
@@ -0,0 +1,13 @@
+package com.locruit.minju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locruit/minju/LocruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locruit.minju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
