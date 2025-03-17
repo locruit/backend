e066407a58f409d176a9a8754b9091bff6f3191b
MinJu Kim <rorrxr@naver.com>
2025-03-17 20:41:05 +0900

    feat: User의 Entity, Dto 추가

---
+++ src/main/java/com/locruit/minju/user/dto/UserInfoDto.java
@@ -0,0 +1,11 @@
+package com.locruit.minju.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoDto {
+    String username;
+    boolean isAdmin;
+}
