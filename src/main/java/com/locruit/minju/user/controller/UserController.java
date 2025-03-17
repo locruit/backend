92a3be461941024f74f62abf81f37f2c10170133
MinJu Kim <rorrxr@naver.com>
2025-03-17 20:42:20 +0900

    feat: 로그인, 회원가입 구현

---
+++ src/main/java/com/locruit/minju/user/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.locruit.minju.user.controller;
+
+import com.locruit.minju.user.dto.SignupRequestDto;
+import com.locruit.minju.user.service.UserService;
+import org.springframework.stereotype.Controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/signup";
+        }
+
+        userService.signup(requestDto);
+
+        return "redirect:/login-page";
+    }
+}
